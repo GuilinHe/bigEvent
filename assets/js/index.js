40817c7d67248701832e62032168414409786b9d
heguilin <15155641890@163.com>
2021-01-29 17:23:43 +0800

    完成了个人中心的基础资料模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,32 @@
+// 获取用户个人信息
+function getUserInfo() {
+    // 发送请求
+    axios.get('/my/userinfo').then(
+        res => {
+            console.log(res);
+            if (res.status !== 0) {
+                return layer.msg("获取用户信息失败！")
+            }
+
+            const { data } = res
+
+            const name = data.nickname || data.username
+
+            $('.nickname').text(`欢迎${name}`).show()
+                // 渲染头像
+            if (data.user_pic) {
+                $('.avatar').prop('src', data.user_pic).show()
+                $('.text-avatar').hide()
+            } else {
+                $('.text-avatar').text(name[0].toUpperCase()).show()
+                $('.avatar').hide()
+            }
+        })
+
+}
 $(function() {
     const { layer } = layui
-    // 获取用户个人信息
-    function getUserInfo() {
-        // 发送请求
-        axios.get('/my/userinfo').then(
-            res => {
-                console.log(res);
-                if (res.status !== 0) {
-                    return layer.msg("获取用户信息失败！")
-                }
 
-                const { data } = res
-
-                const name = data.nickname || data.username
-
-                $('.nickname').text(`欢迎${name}`).show()
-                    // 渲染头像
-                if (data.user_pic) {
-                    $('.avatar').prop('src', data.user_pic).show()
-                    $('.text-avatar').hide()
-                } else {
-                    $('.text-avatar').text(name[0].toUpperCase()).show()
-                    $('.avatar').hide()
-                }
-            })
-
-    }
 
     getUserInfo()
 
